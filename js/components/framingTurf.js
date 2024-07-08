1a84a719e539e9088de1f330a37697e046a5b1d7
sakthivel155 <sakthivelramesh155@gmail.com>
2024-07-08 21:14:06 +0530

    json and grid design

--- js/components/framingTurf.js
+++ js/components/framingTurf.js
@@ -1,7 +1,33 @@
 import { turfs } from '../data/turfDetails.js';
 
 let turfCardContainer = document.getElementById('turf-list-wrapper');
+let turfCardsHTML = '';
 
-turfs.forEach((turfCard)=>{
-    console.log(turfCard);
+turfs.forEach((turfCard) => {
+    turfCardsHTML += `
+    <div class="turf-card">
+        <div class="turf-image">
+            <img src="${turfCard.turf_imgurl1}" alt="Turf Image">
+            <span>Bookable</span>
+        </div>
+        <div class="turf-detail">
+            <div>
+                <div class="turf-name">${turfCard.turf_name}</div>
+                <div class="turf-rating">
+                    <img src="../assets/icons/turf card/star-icon.svg" alt="star-icon">
+                    <div>${turfCard.turf_avg_rating}</div>
+                    <div>(${turfCard.turf_no_of_rating})</div>
+                </div>
+            </div>
+            <div>
+                <div class="turf-place">${turfCard.turf_place}</div>
+                <div>(~ ${turfCard.turf_distance} km)</div>
+            </div>
+           <div class="turf-available-sports">      
+                 ${turfCard.turf_sports.map(sport => `<img src="../assets/icons/turf card/${sport}.png" alt="${sport}">`).join('')}
+           </div>
+        </div>
+    </div>`;
 });
+
+turfCardContainer.innerHTML = turfCardsHTML;
