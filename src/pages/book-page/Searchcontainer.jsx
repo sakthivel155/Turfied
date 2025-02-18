79c7a9dca3da320b39bae54b7daf0e4b41884bae
sakthivel155 <sakthivelramesh155@gmail.com>
2025-02-18 09:33:22 +0530

    test

--- src/pages/book-page/Searchcontainer.jsx
+++ src/pages/book-page/Searchcontainer.jsx
@@ -15,7 +15,7 @@ const SearchContainer = ({ setDisplayTurfs, setCurrentCity ,turfs, setTurfs}) =>
 
   const fetchTurfs = async () => { 
     try {
-      const response = await fetch('http://localhost:3000/api/getTurfs'); 
+      const response = await fetch('https://localhost:3000/api/getTurfs'); 
       const data = await response.json();
       setTurfs(data);
       setDisplayTurfs(data);
